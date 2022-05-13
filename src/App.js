86871f02ec14ccbb88c3a675e062216d0675ab0e
athiragi <athirasnair72476@gmail.com>
2022-05-13 15:40:22 +0530

    messeage

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+  
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">username</label>
+                    <input placeholder="Enter Username" type="text" className="form-control" />
+                
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Password</label>
+                    <input placeholder="Enter Password" type="password" name="" id="" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  );
+}
+
+export default App;
